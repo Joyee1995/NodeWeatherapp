f28d3f7e08f7eda8415a6c5807504c760e5f69b0
Joyee <vickyjoytan@gmail.com>
2020-04-25 16:19:54 +0800

    hbsAsViewengine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,15 +3,22 @@ const express = require('express')
 const app = express()
 const publicdir = path.join(__dirname,'../public')
 
-console.log(__dirname)
-console.log(__filename)
-console.log(path.join(publicdir))
+// console.log(__dirname)
+// console.log(__filename)
+// console.log(path.join(publicdir))
 
-app.use(express.static(path.join(__dirname,'../public')))
+app.set('view engine','hbs')
 
-// app.get('',(req,res)=>{
-//     res.send('<h1>Hello express!</h1>')
-// })
+// app.use(express.static(publicdir))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'Weather App',
+        name: 'Joyee'
+    })
+})
+
+
 
 app.get('/help',(req,res)=>{
     res.send([{
