46c27d057ad6d637dc577c1566b598fd54483fec
Joyee <vickyjoytan@gmail.com>
2020-04-25 17:15:58 +0800

    partialsWithVarInApp

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,25 @@
 const path = require('path')
 const express = require('express')
 const app = express()
+const hbs = require('hbs')
+
 const publicdir = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../template/views')
+const partialPath = path.join(__dirname,'../template/partials')
+// console.log(path.join(__dirname,'../views'))
 
 // console.log(__dirname)
 // console.log(__filename)
 // console.log(path.join(publicdir))
 
+//Setup handlerbars enjine and views location
+// refer app.set at http://expressjs.com/en/4x/api.html#app.set
 app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialPath)
 
-// app.use(express.static(publicdir))
+
+
 
 app.get('',(req,res)=>{
     res.render('index',{
@@ -18,6 +28,17 @@ app.get('',(req,res)=>{
     })
 })
 
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        helpText:'relax',
+        name:"Meg"
+        
+    })
+})
+
+//Setup static directory to serve
+app.use(express.static(publicdir))
 
 
 app.get('/help',(req,res)=>{
