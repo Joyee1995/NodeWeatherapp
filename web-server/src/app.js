1ea9e036c91c163a19720f1dd4ac20ee18f72b10
Joyee <vickyjoytan@gmail.com>
2020-04-25 19:00:08 +0800

    modeifiedFile&query

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,9 +3,9 @@ const express = require('express')
 const app = express()
 const hbs = require('hbs')
 
-const publicdir = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../template/views')
-const partialPath = path.join(__dirname,'../template/partials')
+const publicdir = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../template/views')
+const partialPath = path.join(__dirname, '../template/partials')
 // console.log(path.join(__dirname,'../views'))
 
 // console.log(__dirname)
@@ -14,26 +14,26 @@ const partialPath = path.join(__dirname,'../template/partials')
 
 //Setup handlerbars enjine and views location
 // refer app.set at http://expressjs.com/en/4x/api.html#app.set
-app.set('view engine','hbs')
-app.set('views',viewsPath)
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 hbs.registerPartials(partialPath)
 
 
 
 
-app.get('',(req,res)=>{
-    res.render('index',{
-        title:'Weather App',
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
         name: 'Joyee'
     })
 })
 
-app.get('/help',(req,res)=>{
-    res.render('help',{
-        title:'Help',
-        helpText:'relax',
-        name:"Meg"
-        
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        helpText: 'relax',
+        name: "Meg"
+
     })
 })
 
@@ -41,31 +41,44 @@ app.get('/help',(req,res)=>{
 app.use(express.static(publicdir))
 
 
-app.get('/help',(req,res)=>{
+app.get('/help', (req, res) => {
     res.send([{
         name: 'Andrew',
-        age:27
-    },{
-        name:'Joyee'
+        age: 27
+    }, {
+        name: 'Joyee'
 
     }])
 })
 
-app.get('/weather',(req,res)=>{
-    res.send({
-        forecast: 'It is snowing',
-        location:'Philadelphia'
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Joyee'
     })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) {
+        return res.send({
+            error: 'please provide address'
+        })
+    } res.send({
+        forecast: 'It is snowing',
+        location:'Malaysia',
+        address: req.query.address
+    })
+
 })
 
 
 //*表示其他沒有設route的地址
-app.get('/help/*',(req,res)=>{
-    res.render('404',{
+app.get('/help/*', (req, res) => {
+    res.render('404', {
         errorMessage: 'Help article not found'
     })
 })
-app.get('*',(req,res)=>{
+app.get('*', (req, res) => {
     res.send('PAGE NOT FOUND!')
 })
 
@@ -75,6 +88,6 @@ app.get('*',(req,res)=>{
 //     res.send('404-找不到網頁');
 // })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Server is up on port 3000.')
 })
